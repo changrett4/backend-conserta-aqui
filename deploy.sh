c9ac3797485075dcaffdb6215b3596da72d79bd2
lucas.bastos <bastoslucas1501@gmail.com>
2024-05-27 08:47:15 -0400

    criação script de deploy oci

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+export DOCKER_USERNAME=SECRET1_PLACEHOLDER 
+export DOCKER_PASSWORD=SECRET2_PLACEHOLDER
+export APPLICATION_PORT=SECRET3_PLACEHOLDER
+
+podman login docker.io -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker pull $DOCKER_USERNAME/backend-conserta-aqui:latest
+docker rm -f backend-conserta-aqui
+docker run -d -p $APPLICATION_PORT:3000 --name backend-conserta-aqui $DOCKER_USERNAME/backend-conserta-aqui
