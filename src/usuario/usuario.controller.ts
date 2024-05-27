f73b2687fe0f25f0fa5135211e3571d1e79d7d6c
changrett4 <bastoslucas1501@gmail.com>
2024-05-27 19:12:49 -0400

    continuação login básico

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUsuarioDTO } from './dto/CreateUsuario.dto';
 import { UsuarioService } from './usuario.service';
+import { LoginUsuarioDTO } from './dto/LoginUsuario.dto';
 
 @Controller('/usuarios')
 export class UsuarioController {
@@ -10,4 +11,10 @@ export class UsuarioController {
     async criarUsuario(@Body() dadosUsuario:CreateUsuarioDTO){
         return this.usuarioService.create(dadosUsuario);
     }
+
+    @Post('/login')
+    async loginUsuario(@Body() dadosLogin:LoginUsuarioDTO){
+        console.log(dadosLogin);
+        return this.usuarioService.login(dadosLogin);
+    } 
 }
