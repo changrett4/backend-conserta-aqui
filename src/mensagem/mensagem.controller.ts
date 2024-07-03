580c159729c74ad5440643e389d052c6b052b08a
changrett4 <bastoslucas1501@gmail.com>
2024-07-03 00:36:30 -0400

    att teste e api tag mensagem

--- src/mensagem/mensagem.controller.ts
+++ src/mensagem/mensagem.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { MensagemService } from './mensagem.service';
 import { Public } from '../auth/auth.decorator';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('mensagem')
+@ApiTags('mensagem')
 export class MensagemController {
     constructor(private readonly mensagemService: MensagemService){}
 
