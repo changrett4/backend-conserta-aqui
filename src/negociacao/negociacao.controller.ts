bcfd9936be7d6b316714106f595734b999d993ec
changrett4 <bastoslucas1501@gmail.com>
2024-07-17 00:43:20 -0400

    add api tags negociacao

--- src/negociacao/negociacao.controller.ts
+++ src/negociacao/negociacao.controller.ts
@@ -3,8 +3,10 @@ import { NegociacaoService } from './negociacao.service';
 import { CreateNegociacaoDto } from './dto/create-negociacao.dto';
 import { UpdateNegociacaoDto } from './dto/update-negociacao.dto';
 import { Public } from '../auth/auth.decorator';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('negociacao')
+@ApiTags('negociacao')
 export class NegociacaoController {
   constructor(private readonly negociacaoService: NegociacaoService) {}
 
